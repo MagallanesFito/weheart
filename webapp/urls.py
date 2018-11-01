ffadf742eb826937cfff26a0640352906c8a7ba4
MagallanesFito <magallanesfito@gmail.com>
2018-11-01 18:10:17 +0100

    Agregado esqueleto basico de aplicacion

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'webapp'
 urlpatterns = [
-	path('',views.index,name="index")
+	path('',views.index,name="index"), #index incluye el formulario de log in
+	path('register/',views.register,name="register"),
+	path('dashboard/',views.dashboard,name="dashboard"),
+	path('me/',views.me,name="me")
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
 	return render(request,'webapp/index.html')
+	#return render(request,'webapp/index.html')
+def dashboard(request):
+	return render(request,'webapp/dashboard.html')
+def register(request):
+	return render(request,'webapp/register.html')
+def me(request):
+	return render(request,'webapp/me.html')
