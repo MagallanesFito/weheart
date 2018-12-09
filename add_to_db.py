d02ce9897a9b54881fc199013750f17148478d7a
MagallanesFito <magallanesfito@gmail.com>
2018-12-09 23:57:51 +0100

    Modificada hobbies list

--- add_to_db.py
+++ add_to_db.py
@@ -1,6 +1,36 @@
 from webapp.models import Interest
 
+#Se cargan los hobbies en espanol
+def procesar(cadena):
+	procesada = str(cadena).title()
+	resumida = procesada.split("(")[0]
+	resumida = resumida.split(":")[0]
+	result = resumida
+	if resumida[-1] == ' ':
+		result = resumida[:-1]
+	if result[-1] != '\n':
+		result+='\n'
+	return result
+hobbies_list = []
+file = open("hobbies_spanish.txt",'r')
+for line in file:
+	procesado = procesar(line)
+	if procesado not in hobbies_list:
+		hobbies_list.append(procesado)
+#hobbies en ingles
+file.close()
 file = open("hobbies.txt",'r')
 for line in file:
-	p = Interest(name=str(line))
+	procesado = procesar(line)
+	if procesado not in hobbies_list:
+		hobbies_list.append(procesado)
+file.close()
+
+#La lista debe mostrar los hobbies ordenados para facilitar la busqueda
+file = open("hobbies_list.txt",'w')
+hobbies_list.sort()
+for hobbie in hobbies_list:
+	file.write(hobbie)
+	p = Interest(name=str(hobbie))
 	p.save()
+file.close()
